6cee8f0535a89f17175ee65d368f2c3e8796ead3
MartinPerezGiordan <martin.perezgiordan@gmail.com>
2023-05-24 15:21:46 -0300

    Add buttons functionality

--- code.js
+++ code.js
@@ -7,23 +7,34 @@ const cardContainer = document.querySelector('.card-container')
 const titleInput = document.querySelector('#bookTitle')
 const authorInput = document.querySelector('#bookAuthor')
 const pagesInput = document.querySelector('#bookPages')
+const readInput = document.querySelector('#bookRead')
 const submitBtn = document.querySelector('#submitBook')
 
 
+let bookId = 1
 
 let myLibrary = [
-    {title:'Hobbit',author:'Tolkien',pages:230},{title:'Narnia',author:'Pepe',pages:230}
+    new Book('Hobbit','Tolkien',230,true,1),new Book('Narnia','Pepe',230,true,2)
 ]
 console.log(myLibrary)
 
-function Book(title,author,pages){
+function Book(title,author,pages,read){
     this.title=title
     this.author=author
     this.pages=pages
+    this.read=read
+    this.bookId=bookId
+    bookId++
 }
 
-function addBookToLibrary(titleInput,authorInput,pagesInput){
-    let book = new Book(titleInput,authorInput,pagesInput)
+function addBookToLibrary(titleInput,authorInput,pagesInput,readInput){
+    let userReadIt = true;
+    if(!readInput){
+        userReadIt = false;
+    }
+
+    let book = new Book(titleInput,authorInput,pagesInput,userReadIt)
+    console.log(book)
     myLibrary.push(book)
 }
 
@@ -33,11 +44,13 @@ function submitBtnListener(){
         let titleInputValue = titleInput.value
         let authorInputValue = authorInput.value
         let pagesInputValue = pagesInput.value
-        addBookToLibrary(titleInputValue, authorInputValue, pagesInputValue)
+        let readInputValue = readInput.checked
+        addBookToLibrary(titleInputValue, authorInputValue, pagesInputValue, readInputValue)
         displayCards();
         titleInput.value = ''
         authorInput.value = ''
         pagesInput.value = ''
+        readInput.checked = false
         newBookForm.style.display = 'none';
     })
 }
@@ -47,14 +60,27 @@ submitBtnListener();
 function displayCards(){
     cardContainer.innerHTML=''
     myLibrary.forEach(book => {
-        let title = `<h2>${book.title}</h2>`
-        let author = `<p>${book.author}</p>`
-        let pages = `<p>${book.pages}</p>`
-        let card = document.createElement('div')
-        card.innerHTML = title + author + pages
-        cardContainer.appendChild(card)
-        card.classList.add('card')
+        if(!book.disabled){
+            let title = `<h2>${book.title}</h2>`
+            let author = `<p>${book.author}</p>`
+            let pages = `<p>${book.pages}</p>`
+            let btnRead = `<button class='read readBtn' id='${book.bookId}'>Read</button>`
+            if(!book.read){
+                btnRead = `<button class='not-read readBtn' id='${book.bookId}'>Not Read</button>`
+            }
+
+            let btnDelete = `<button class='deleteBtn' id='${book.bookId}'>Delete</button>`
+
+            let card = document.createElement('div')
+            card.innerHTML = title + author + pages+btnRead + btnDelete;
+            cardContainer.appendChild(card)
+            card.classList.add('card')
+        }
     });
+changeReadListener();
+deleteBookListener();
+
+
 }
 displayCards();
 
@@ -78,7 +104,59 @@ function hideFormForNewBook(){
 hideFormForNewBook();
 
 
+// ---------------- READ BUTTON ---------------------//
+function changeReadListener(){
+    const readBtns = document.querySelectorAll('.readBtn')
 
+    readBtns.forEach((readBtn)=>{
+        readBtn.addEventListener('click', (e)=>{
+            console.log(e.target)
+            changeReadInObject(readBtn.id);
+            displayCards();
+            console.log(myLibrary)
+        })
+    })
+   
+}
+
+Book.prototype.toggleReadStatus = function(){
+    this.read = !this.read;
+}
+
+function changeReadInObject(id){
+    console.log(id)
+    myLibrary.forEach(book => {
+
+        if(id==book.bookId){
+            book.toggleReadStatus();
+        }
+        
+    });
+}
+
+// ---------------- DELETE BUTTON ---------------------//
+function deleteBookListener(){
+    const deleteBtns = document.querySelectorAll('.deleteBtn')
+
+    deleteBtns.forEach((deleteBtn)=>{
+        deleteBtn.addEventListener('click', (e)=>{
+            console.log(e.target)
+            deleteBook(deleteBtn.id);
+        })
+    })
+}
+deleteBookListener();
+
+function deleteBook(id){
+    myLibrary.forEach(book=>{
+        if(id==book.bookId){
+            book.disabled = true;
+            console.log(book)
+        }
+    })
+    displayCards();
+
+}
 
 
 
